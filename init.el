33a191c2faaffdd9c4cf1a1bff41b3a2ffb5b058
Kreedzt <zhaozisong1@live.com>
2019-08-20 20:29:45 +0800

    Kreedzt - disable conflict-elisp keymap && add company-box

--- init.el
+++ init.el
@@ -118,7 +118,7 @@ This function should only modify configuration layer settings."
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
   
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(company-box)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -568,6 +568,9 @@ before packages are loaded."
     )
   (my-setup-indent 2)
 
+  (require 'company-box)
+  (add-hook 'company-mode-hook 'company-box-mode)
+
   "modify typescript indent level"
   (setq-default typescript-indent-level 2)
   "Eslint fix file"
@@ -710,13 +713,8 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(lsp-enable-indentation nil)
  '(package-selected-packages
    (quote
     (web-mode tagedit slim-mode scss-mode sass-mode pug-mode impatient-mode helm-css-scss helm helm-core haml-mode emmet-mode counsel-css company-web web-completion-data add-node-modules-path wakatime-mode yasnippet-snippets ws-butler writeroom-mode winum which-key wgrep web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package unfill toc-org symon string-inflection spaceline-all-the-icons solarized-theme smex smeargle rjsx-mode restart-emacs request rainbow-delimiters prettier-js popwin persp-mode pcre2el password-generator paradox overseer orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets org-brain open-junk-file neotree nameless mwim move-text mmm-mode markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum livid-mode linum-relative link-hint json-navigator json-mode js2-refactor js-doc ivy-yasnippet ivy-xref ivy-purpose ivy-hydra indent-guide hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-make google-translate golden-ratio gnuplot gitignore-templates gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md fuzzy font-lock+ flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-org evil-numbers evil-nerd-commenter evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu elisp-slime-nav editorconfig dumb-jump dotenv-mode doom-modeline diminish define-word counsel-projectile company-tern company-statistics column-enforce-mode clean-aindent-mode centered-cursor-mode auto-yasnippet auto-highlight-symbol auto-compile aggressive-indent ace-window ace-link ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 )
--- layers/zilongshanren-programming/packages.el
+++ layers/zilongshanren-programming/packages.el
@@ -16,7 +16,7 @@
       '(
         css-mode
         ;; paredit
-        lispy
+        ;; lispy
         caps-lock
         ;; cmake-font-lock
         ;; cmake-mode
@@ -35,7 +35,7 @@
         ;; flycheck-clojure
         ;; etags-select
         ;; (python :location built-in)
-        (emacs-lisp :location built-in)
+        ;; (emacs-lisp :location built-in)
         ;; clojure-mode
         company
         (eldoc :location built-in)
@@ -211,40 +211,40 @@
 (defun zilongshanren-programming/init-flycheck-package ()
   (use-package flycheck-package))
 
-(defun zilongshanren-programming/init-lispy ()
-  (use-package lispy
-    :defer t
-    :init
-    (progn
-      (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
-      (add-hook 'ielm-mode-hook (lambda () (lispy-mode 1)))
-      (add-hook 'inferior-emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
-      ;; (add-hook 'spacemacs-mode-hook (lambda () (lispy-mode 1)))
-      (add-hook 'clojure-mode-hook (lambda () (lispy-mode 1)))
-      (add-hook 'scheme-mode-hook (lambda () (lispy-mode 1)))
-      (add-hook 'cider-repl-mode-hook (lambda () (lispy-mode 1)))
-      )
-    ;; :config
-    ;; (progn
-    ;;   (push '(cider-repl-mode . ("[`'~@]+" "#" "#\\?@?")) lispy-parens-preceding-syntax-alist)
+;; (defun zilongshanren-programming/init-lispy ()
+;;   (use-package lispy
+;;     :defer t
+;;     :init
+;;     (progn
+;;       (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+;;       (add-hook 'ielm-mode-hook (lambda () (lispy-mode 1)))
+;;       (add-hook 'inferior-emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+;;       ;; (add-hook 'spacemacs-mode-hook (lambda () (lispy-mode 1)))
+;;       (add-hook 'clojure-mode-hook (lambda () (lispy-mode 1)))
+;;       (add-hook 'scheme-mode-hook (lambda () (lispy-mode 1)))
+;;       (add-hook 'cider-repl-mode-hook (lambda () (lispy-mode 1)))
+;;       )
+;;     ;; :config
+;;     ;; (progn
+;;     ;;   (push '(cider-repl-mode . ("[`'~@]+" "#" "#\\?@?")) lispy-parens-preceding-syntax-alist)
 
-    ;;   (spacemacs|hide-lighter lispy-mode)
-    ;;   (define-key lispy-mode-map (kbd "M-s") 'lispy-splice)
-    ;;   (define-key lispy-mode-map (kbd "s-k") 'paredit-splice-sexp-killing-backward)
+;;     ;;   (spacemacs|hide-lighter lispy-mode)
+;;     ;;   (define-key lispy-mode-map (kbd "M-s") 'lispy-splice)
+;;     ;;   (define-key lispy-mode-map (kbd "s-k") 'paredit-splice-sexp-killing-backward)
 
-    ;;   (with-eval-after-load 'cider-repl
-    ;;     (define-key cider-repl-mode-map (kbd "C-s-j") 'cider-repl-newline-and-indent))
+;;     ;;   (with-eval-after-load 'cider-repl
+;;     ;;     (define-key cider-repl-mode-map (kbd "C-s-j") 'cider-repl-newline-and-indent))
 
-    ;;   (add-hook
-    ;;    'minibuffer-setup-hook
-    ;;    'conditionally-enable-lispy)
-    ;;   (define-key lispy-mode-map (kbd "s-m") 'lispy-mark-symbol)
-    ;;   (define-key lispy-mode-map (kbd "s-u") 'lispy-undo)
-    ;;   (define-key lispy-mode-map (kbd "s-1") 'lispy-describe-inline)
-    ;;   (define-key lispy-mode-map (kbd "s-2") 'lispy-arglist-inline)
-    ;;   )
-    )
-  )
+;;     ;;   (add-hook
+;;     ;;    'minibuffer-setup-hook
+;;     ;;    'conditionally-enable-lispy)
+;;     ;;   (define-key lispy-mode-map (kbd "s-m") 'lispy-mark-symbol)
+;;     ;;   (define-key lispy-mode-map (kbd "s-u") 'lispy-undo)
+;;     ;;   (define-key lispy-mode-map (kbd "s-1") 'lispy-describe-inline)
+;;     ;;   (define-key lispy-mode-map (kbd "s-2") 'lispy-arglist-inline)
+;;     ;;   )
+;;     )
+;;   )
 
 (defun zilongshanren-programming/post-init-flycheck ()
   (with-eval-after-load 'flycheck
